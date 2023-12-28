60a325b3c375591575a370f92250d5bb616aae5a
Abhimnayu kumar <52759159+veeroo2011@users.noreply.github.com>
2023-12-28 18:16:39 +0530

    Update remote_backend_s3.tf

--- infra/remote_backend_s3.tf
+++ infra/remote_backend_s3.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "dev-proj-1-remote-state-bucket-123456"
+    bucket = "dev-proj-1-jenkins-remote-state-bucket-12345678"
     key    = "devops-project-1/terraform.tfstate"
     region = "eu-central-1"
   }
