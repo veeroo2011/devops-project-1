274c5b110bfe621b94df7582c550b50cbd799982
Abhimnayu kumar <52759159+veeroo2011@users.noreply.github.com>
2023-12-28 20:20:20 +0530

    Update main.tf

--- infra/ec2/main.tf
+++ infra/ec2/main.tf
@@ -23,7 +23,7 @@ resource "aws_instance" "dev_proj_1_ec2" {
   tags = {
     Name = var.tag_name
   }
-  key_name                    = "aws_key"
+  key_name                    = "test"
   subnet_id                   = var.subnet_id
   vpc_security_group_ids      = [var.sg_enable_ssh_https, var.ec2_sg_name_for_python_api]
   associate_public_ip_address = var.enable_public_ip_address
