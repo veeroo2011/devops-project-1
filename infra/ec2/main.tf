2546aebaca645ef752cdc4023466f2aeb9d9516b
Abhimnayu kumar <52759159+veeroo2011@users.noreply.github.com>
2023-12-28 18:52:35 +0530

    Update main.tf

--- infra/ec2/main.tf
+++ infra/ec2/main.tf
@@ -8,9 +8,10 @@ variable "enable_public_ip_address" {}
 variable "user_data_install_apache" {}
 variable "ec2_sg_name_for_python_api" {}
 
-output "ssh_connection_string_for_ec2" {
+/*output "ssh_connection_string_for_ec2" {
   value = format("%s%s", "ssh -i /home/ubuntu/keys/aws_ec2_terraform ubuntu@", aws_instance.dev_proj_1_ec2.public_ip)
 }
+*/
 
 output "dev_proj_1_ec2_instance_id" {
   value = aws_instance.dev_proj_1_ec2.id
@@ -35,7 +36,8 @@ resource "aws_instance" "dev_proj_1_ec2" {
   }
 }
 
-resource "aws_key_pair" "dev_proj_1_public_key" {
+/*resource "aws_key_pair" "dev_proj_1_public_key" {
   key_name   = "aws_key"
   public_key = var.public_key
 }
+*/
