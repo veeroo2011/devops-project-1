b35bc1d1ccd4f4536114aa6d14cd3f7a95229c88
Abhimnayu kumar <52759159+veeroo2011@users.noreply.github.com>
2023-12-28 18:14:11 +0530

    Update main.tf

--- infra/load-balancer/main.tf
+++ infra/load-balancer/main.tf
@@ -9,9 +9,9 @@ variable "ec2_instance_id" {}
 variable "lb_listner_port" {}
 variable "lb_listner_protocol" {}
 variable "lb_listner_default_action" {}
-variable "lb_https_listner_port" {}
-variable "lb_https_listner_protocol" {}
-variable "dev_proj_1_acm_arn" {}
+//variable "lb_https_listner_port" {}
+//variable "lb_https_listner_protocol" {}
+//variable "dev_proj_1_acm_arn" {}
 variable "lb_target_group_attachment_port" {}
 
 output "aws_lb_dns_name" {
